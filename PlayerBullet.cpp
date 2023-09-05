555f4bc07ef3b4cb000977fd02289efc989a671f
okmrng <k022g1055@g.neec.ac.jp>
2023-09-05 15:40:19 +0900

    自機の弾クラス作成

---
+++ PlayerBullet.cpp
@@ -0,0 +1,20 @@
+﻿#include "PlayerBullet.h"
+
+void PlayerBullet::Initialize(Vector2 pos)
+{
+	radius_ = 10.0f;
+	pos_ = pos;
+	velocity_ = 20.0f;
+	isDead_ = false;
+}
+
+void PlayerBullet::Update()
+{
+	// 移動
+	pos_.x += velocity_;
+}
+
+void PlayerBullet::Draw()
+{
+	Novice::DrawEllipse(int(pos_.x), int(pos_.y), int(radius_), int(radius_), 0.0f, RED, kFillModeSolid);
+}
---
+++ PlayerBullet.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include <Vector2.h>
+#include <Novice.h>
+
+/// <summary>
+/// 自機に弾
+/// </summary>
+class PlayerBullet
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(Vector2 pos);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	//ゲッター
+	bool GetIsDead() { return isDead_; }
+	
+private:
+	float radius_;   // 半径
+	Vector2 pos_;    // 座標
+	float velocity_; // 速度
+	bool isDead_;    // デスフラグ
+};
+
