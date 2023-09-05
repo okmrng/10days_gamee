0fb6b8efb0763935f9ff81cd29495f384b8f9db3
okmrng <k022g1055@g.neec.ac.jp>
2023-09-05 15:22:22 +0900

    自機の処理追加

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,32 @@
 #include "Player.h"
+
+void Player::Initialize()
+{
+	radius_ = 10.0f;
+	pos_ = Vector2{ radius_ + 10.0f,360.0f };
+	velocity_ = 5.0f;
+}
+
+void Player::Upadate(char* keys, char* preKeys)
+{
+	// ˆÚ“®
+	if (keys[DIK_DOWN]) {
+		pos_.y += velocity_;
+	}
+	if (keys[DIK_UP]) {
+		pos_.y -= velocity_;
+	}
+
+	// ”ÍˆÍ§ŒÀ
+	if (pos_.y >= 720.0f - radius_) {
+		pos_.y = 720.0f - radius_;
+	}
+	if (pos_.y <= radius_) {
+		pos_.y = radius_;
+	}
+}
+
+void Player::Draw()
+{
+	Novice::DrawEllipse(int(pos_.x), int(pos_.y), int(radius_), int(radius_), 0.0f, BLUE, kFillModeSolid);
+}
--- Player.h
+++ Player.h
@@ -1,6 +1,33 @@
 #pragma once
+
+#include <Vector2.h>
+#include <Novice.h>
+
+/// <summary>
+/// 自機クラス
+/// </summary>
 class Player
 {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
 
+	/// <summary>
+	///  更新
+	/// </summary>
+	void Upadate(char* keys, char* preKeys);
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+	// 自機情報
+	float radius_;     // 半径
+	Vector2 pos_;      // 座標
+	float velocity_;   // 速度
 };
 
