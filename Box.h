bfa7a16e5fbe28ca41c1bc6912e7a5a82e27f2b1
okmrng <k022g1055@g.neec.ac.jp>
2023-09-05 16:30:23 +0900

    敵クラス作成

---
+++ Box.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include <Vector2.h>
+
+/// <summary>
+/// 押す箱
+/// </summary>
+class Box
+{
+//public:
+//	/// <summary>
+//	/// 初期化
+//	/// </summary>
+//	void Initialize(Vector2 pos);
+//
+//	/// <summary>
+//	/// 更新
+//	/// </summary>
+//	void Update();
+//
+//	/// <summary>
+//	/// 描画
+//	/// </summary>
+//	void Draw();
+//
+//	// ゲッター
+//	bool GetIsDead() { return isDead_; }
+//
+//private:
+//	Vector2 pos_; // 座標
+//	bool isDead_; // デスフラグ
+};
+
