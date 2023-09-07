f4ff2926bc6941b52dc656ac0206bf9f7741d2e2
okmrng <k022g1055@g.neec.ac.jp>
2023-09-07 13:50:54 +0900

    ゴール調整

--- Goal.cpp
+++ Goal.cpp
@@ -3,10 +3,11 @@
 void Goal::Initialize(float posY)
 {
 	pos_ = Vector2{ 1000.0f,posY };
-	size_ = Vector2{ 100.0f,100.0f };
+	size_ = Vector2{ 50.0f,50.0f };
 }
 
 void Goal::Draw()
 {
-	Novice::DrawBox(int(pos_.x), int(pos_.y), int(size_.x), int(size_.y), 0.0f, GREEN, kFillModeWireFrame);
+	Novice::DrawBox(int(pos_.x + 25), int(pos_.y + 25), int(size_.x), int(size_.y), 0.0f, 
+		GREEN, kFillModeWireFrame);
 }
