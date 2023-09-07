16504841c86f70f4541d9863e27471d7c429139e
okmrng <k022g1055@g.neec.ac.jp>
2023-09-07 15:58:46 +0900

    クリアクラス作成

---
+++ Clear.cpp
@@ -0,0 +1,9 @@
+﻿#include "Clear.h"
+
+void Clear::Draw()
+{
+	// デバッグテキスト
+	#ifdef _DEBUG
+	Novice::ScreenPrintf(0, 80, "Clear");
+	#endif // _DEBUG
+}
---
+++ Clear.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <Novice.h>
+
+/// <summary>
+/// クリア
+/// </summary>
+class Clear
+{
+public:
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+};
+
