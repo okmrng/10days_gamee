0c64586f88ba14653b8ba0dda25b2e84f112d542
okmrng <k022g1055@g.neec.ac.jp>
2023-09-07 12:51:32 +0900

    easeOutCirc関数作成

--- MathUtility.cpp
+++ MathUtility.cpp
@@ -1,5 +1,21 @@
 ﻿#include "MathUtility.h"
 
+float EaseOutCirc(float pos, float startPoint, float endPoint, float t)
+{
+	if (t < 1.0f) {
+		t += 1.0f / 30.0f;
+	}
+	if (t >= 1.0f) {
+		t = 1.0f;
+	}
+
+	float easedT_ = sqrt(1.0f - pow(t - 1.0f, 2.0f));
+
+	pos = (1.0f - easedT_) * startPoint + easedT_ * endPoint;
+
+	return pos;
+}
+
 Vector2& operator-=(Vector2& v, float s) {
 	v.x -= s;
 	v.y -= s;
