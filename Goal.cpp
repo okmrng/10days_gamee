04a007a9dbff6078dde5bf0fa0083c725cdd8102
okmrng <k022g1055@g.neec.ac.jp>
2023-09-07 11:49:55 +0900

    ゴールクラス作成

---
+++ Goal.cpp
@@ -0,0 +1,12 @@
+﻿#include "Goal.h"
+
+void Goal::Initialize(float posY)
+{
+	pos_ = Vector2{ 1000.0f,posY };
+	size_ = Vector2{ 100.0f,100.0f };
+}
+
+void Goal::Draw()
+{
+	Novice::DrawBox(int(pos_.x), int(pos_.y), int(size_.x), int(size_.y), 0.0f, WHITE, kFillModeWireFrame);
+}
---
+++ Goal.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <Vector2.h>
+#include <Novice.h>
+
+class Goal
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="posY">ゴールのY座標</param>
+	void Initialize(float posY);
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+	// ゴール
+	Vector2 pos_;  // 座標
+	Vector2 size_; //幅
+};
+
