3171f833a8497f28b803bd25b73f698e4c8cd7f8
okmrng <k022g1055@g.neec.ac.jp>
2023-09-07 21:12:11 +0900

    タイトルクラス作成

---
+++ Title.cpp
@@ -0,0 +1,34 @@
+﻿#include "Title.h"
+
+void Title::Initialize()
+{
+	// 画像
+	texture_ = Novice::LoadTexture("./resource/background/title.png");
+	color_ = 0xffffff;
+	alpha_ = 0x000002;
+	isFade_ = false;
+
+	toNext_ = false;
+}
+
+void Title::Update(char* keys)
+{
+	if (keys[DIK_SPACE]) {
+		isFade_ = true;
+	}
+
+	// フェードアウト
+	if (isFade_) {
+		color_ -= alpha_;
+	}
+
+	// フェードアウトしきったら次のシーンへ
+	if (color_ <= 0x000000) {
+		toNext_ = true;
+	}
+}
+
+void Title::Draw()
+{
+	Novice::DrawSprite(0, 0, texture_, 1, 1, 0.0f, color_);
+}
---
+++ Title.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include <stdint.h>
+#include <Novice.h>
+
+/// <summary>
+/// タイトル
+/// </summary>
+class Title
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	/// <param name="keys">キー</param>
+	void Update(char* keys);
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	// ゲッター
+	bool GetToNext() { return toNext_; }
+
+private:
+	// 画像
+	uint32_t texture_; // テクスチャハンドル
+	uint32_t color_;   // 色
+	uint32_t alpha_;   // 透明度
+	bool isFade_;      // フェードアウトフラグ
+
+	bool toNext_; // 次のシーンへ遷移するためのフラグ
+};
+
