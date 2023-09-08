9454a3e902747cafb0bd330f49b9f6c6c73f6599
okmrng <k022g1055@g.neec.ac.jp>
2023-09-09 01:15:42 +0900

    ポーズ画面のUIの基本位置設定

--- Pause.cpp
+++ Pause.cpp
@@ -2,6 +2,7 @@
 
 void Pause::Initialize()
 {
+	// 文字
 	PPos_ = Vector2(392.5f, 100.0f);
 	APos_ = Vector2(493.5f, 100.0f);
 	UPos_ = Vector2(594.5f, 100.0f);
@@ -13,6 +14,22 @@ void Pause::Initialize()
 	UTexture_ = Novice::LoadTexture("./resource/sprite/U.png");
 	STexture_ = Novice::LoadTexture("./resource/sprite/S.png");
 	ETexture_ = Novice::LoadTexture("./resource/sprite/E.png");
+
+	// UI
+	playPos_ = Vector2(528.0f, 271.0f);
+	retryPos_ = Vector2(505.5f, 375.0f);
+	enemyInfoPos_ = Vector2(414.0f, 479.0f);
+
+	playTexture_ = Novice::LoadTexture("./resource/UI/play.png");
+	retryTexture_ = Novice::LoadTexture("./resource/UI/retry.png");
+	enemyInfoTexture_ = Novice::LoadTexture("./resource/UI/enemyInfo.png");
+	choosePlayTexture_ = Novice::LoadTexture("./resource/UI/play_choose.png");
+	chooseRetryTexture_ = Novice::LoadTexture("./resource/UI/retry_choose.png");
+	chooseEnemyInfoTexture_ = Novice::LoadTexture("./resource/UI/enemyInfo_choose.png");
+
+	choosePlay_ = false;
+	chooseRetry_ = false;
+	chooseEnemyInfo_ = false;
 }
 
 void Pause::Draw()
@@ -23,4 +40,26 @@ void Pause::Draw()
 	Novice::DrawSprite(int(UPos_.x), int(UPos_.y), UTexture_, 1, 1, 0.0f, WHITE);
 	Novice::DrawSprite(int(SPos_.x), int(SPos_.y), STexture_, 1, 1, 0.0f, WHITE);
 	Novice::DrawSprite(int(EPos_.x), int(EPos_.y), ETexture_, 1, 1, 0.0f, WHITE);
+
+	// UI
+	if (!choosePlay_) {
+		Novice::DrawSprite(int(playPos_.x), int(playPos_.y), playTexture_, 1, 1, 0.0f, WHITE);
+	}
+	else if (choosePlay_) {
+		Novice::DrawSprite(int(playPos_.x), int(playPos_.y), choosePlayTexture_, 1, 1, 0.0f, WHITE);
+	}
+
+	if (!chooseRetry_) {
+		Novice::DrawSprite(int(retryPos_.x), int(retryPos_.y), retryTexture_, 1, 1, 0.0f, WHITE);
+	}
+	else if (chooseRetry_) {
+		Novice::DrawSprite(int(retryPos_.x), int(retryPos_.y), chooseRetryTexture_, 1, 1, 0.0f, WHITE);
+	}
+
+	if (!chooseEnemyInfo_) {
+		Novice::DrawSprite(int(enemyInfoPos_.x), int(enemyInfoPos_.y), enemyInfoTexture_, 1, 1, 0.0f, WHITE);
+	}
+	else if (chooseEnemyInfo_) {
+		Novice::DrawSprite(int(enemyInfoPos_.x), int(enemyInfoPos_.y), chooseEnemyInfoTexture_, 1, 1, 0.0f, WHITE);
+	}
 }
--- Pause.h
+++ Pause.h
@@ -32,10 +32,27 @@ private:
 	Vector2 UPos_;      // Uの座標
 	Vector2 SPos_;      // Sの座標
 	Vector2 EPos_;      // Eの座標
+
 	uint32_t PTexture_; // Pのテクスチャハンドル
 	uint32_t ATexture_; // Aのテクスチャハンドル
 	uint32_t UTexture_; // Uのテクスチャハンドル
 	uint32_t STexture_; // Sのテクスチャハンドル
 	uint32_t ETexture_; // Eのテクスチャハンドル
+
+	// UI
+	Vector2 playPos_;				  // 再開の位置
+	Vector2 retryPos_;				  // リトライの位置
+	Vector2 enemyInfoPos_;			  // 敵情報の位置
+									  
+	uint32_t playTexture_;			  // 再開のテクスチャハンドル
+	uint32_t retryTexture_;			  // リトライのテクスチャハンドル
+	uint32_t enemyInfoTexture_;		  // 敵情報のテクスチャハンドル
+	uint32_t choosePlayTexture_;     // 再開選択中のテクスチャハンドル
+	uint32_t chooseRetryTexture_;     // リトライ選択中のテクスチャハンドル
+	uint32_t chooseEnemyInfoTexture_; // 敵情報選択中のテクスチャハンドル
+
+	bool choosePlay_;				  // 再開選択中フラグ
+	bool chooseRetry_;				  // リトライ選択中フラグ
+	bool chooseEnemyInfo_;			  // 敵情報選択中フラグ
 };
 
